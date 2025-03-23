02ea21ec7d8637951ccf6f98625c495e8006daed
SomeRandomNoobKekeke <122838333+SomeRandomNoobKekeke@users.noreply.github.com>
2025-03-23 11:01:54 +0300

    0.2.6.3

--- docs/search/all_14.js
+++ docs/search/all_14.js
@@ -9,11 +9,12 @@ var searchData=
   ['pathing_3a_6',['Temporary solution to pathing:',['../md__c_sharp_2_client_2_crab_u_i_2_changelog.html#autotoc_md10',1,'']]],
   ['paths_20are_20resolved_7',['How texture paths are resolved',['../_sprites.html#autotoc_md35',1,'']]],
   ['pgnassets_8',['PGNAssets',['../class_crab_u_i_1_1_c_u_i.html#a7ad58022298f051fe199bca6ab39df77',1,'CrabUI::CUI']]],
-  ['prepend_9',['Prepend',['../class_crab_u_i_1_1_c_u_i_component.html#a21431d1bcb98626c736124039177b6c7',1,'CrabUI::CUIComponent']]],
-  ['properties_10',['CUISprite Properties',['../_sprites.html#autotoc_md33',1,'']]],
-  ['properties_3a_11',['Properties:',['../_animations.html#autotoc_md28',1,'']]],
-  ['property_12',['Property',['../class_crab_u_i_1_1_c_u_i_animation.html#a7d0f929157c505bfb17374b238c5551f',1,'CrabUI::CUIAnimation']]],
-  ['props_13',['Props',['../_graphic_props.html',1,'Graphic Props'],['../_layout_props.html',1,'Layout Props'],['../_other_props.html',1,'Other Props'],['../class_crab_u_i_1_1_c_u_i_style.html#a49074c884e6c899040314e3386115bb5',1,'CrabUI.CUIStyle.Props']]],
-  ['props_3a_14',['Props:',['../index.html#autotoc_md23',1,'']]],
-  ['proptest_15',['PropTest',['../class_crab_u_i_test_1_1_prop_test.html',1,'CrabUITest']]]
+  ['pianokey_9',['PianoKey',['../class_crab_u_i_test_1_1_complex_test_1_1_piano_key.html',1,'CrabUITest::ComplexTest']]],
+  ['prepend_10',['Prepend',['../class_crab_u_i_1_1_c_u_i_component.html#a21431d1bcb98626c736124039177b6c7',1,'CrabUI::CUIComponent']]],
+  ['properties_11',['CUISprite Properties',['../_sprites.html#autotoc_md33',1,'']]],
+  ['properties_3a_12',['Properties:',['../_animations.html#autotoc_md28',1,'']]],
+  ['property_13',['Property',['../class_crab_u_i_1_1_c_u_i_animation.html#a7d0f929157c505bfb17374b238c5551f',1,'CrabUI::CUIAnimation']]],
+  ['props_14',['Props',['../_graphic_props.html',1,'Graphic Props'],['../_layout_props.html',1,'Layout Props'],['../_other_props.html',1,'Other Props'],['../class_crab_u_i_1_1_c_u_i_style.html#a49074c884e6c899040314e3386115bb5',1,'CrabUI.CUIStyle.Props']]],
+  ['props_3a_15',['Props:',['../index.html#autotoc_md23',1,'']]],
+  ['proptest_16',['PropTest',['../class_crab_u_i_test_1_1_prop_test.html',1,'CrabUITest']]]
 ];
--- docs/search/classes_6.js
+++ docs/search/classes_6.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['proptest_0',['PropTest',['../class_crab_u_i_test_1_1_prop_test.html',1,'CrabUITest']]]
+  ['pianokey_0',['PianoKey',['../class_crab_u_i_test_1_1_complex_test_1_1_piano_key.html',1,'CrabUITest::ComplexTest']]],
+  ['proptest_1',['PropTest',['../class_crab_u_i_test_1_1_prop_test.html',1,'CrabUITest']]]
 ];

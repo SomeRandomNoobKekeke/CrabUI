7c67899522bcfea4959311a85035a41bcbc47059
SomeRandomNoobKekeke <SkritniyChel34617@proton.me>
2026-03-13 20:38:30 +0300

    fixed mem leak in PluginLifeCycle, should also update it everywhere

--- CSharp/Client/Libs/PluginLifeCycle.cs
+++ CSharp/Client/Libs/PluginLifeCycle.cs
@@ -13,16 +13,27 @@ using System.Text;
 namespace BaroJunk
 {
 
-  public static class PluginLifeCycle
+  public class PluginLifeCycle
   {
+    private static PluginLifeCycle Instance;
+
     static PluginLifeCycle()
     {
+      Instance = new PluginLifeCycle();
+
       GameMain.LuaCs.Hook.Add("stop", $"[{ModInfo.AssemblyName}] PluginLifeCycle.End", (object[] args) =>
       {
-        End?.Invoke();
+        Instance?.end?.Invoke();
+        Instance = null;
         return null;
       });
     }
-    public static event Action End;
+    private event Action end;
+
+    public static event Action End
+    {
+      add { if (Instance is not null) Instance.end += value; }
+      remove { if (Instance is not null) Instance.end -= value; }
+    }
   }
 }

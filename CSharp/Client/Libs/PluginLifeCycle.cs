70ca3c59c837228fbc4d54079420afc7e157d7d0
SomeRandomNoobKekeke <SkritniyChel34617@proton.me>
2026-03-16 07:30:31 +0300

    i don't get why lua don't want to patch it

--- CSharp/Client/Libs/PluginLifeCycle.cs
+++ CSharp/Client/Libs/PluginLifeCycle.cs
@@ -20,87 +20,14 @@ namespace BaroJunk
   /// </summary>
   public class PluginLifeCycle
   {
-    public class Hook
-    {
-      public bool IsInstalled { get; private set; }
-      public Action InstallAction { get; set; }
-      public Action RemoveAction { get; set; }
-
-      public void TryInstall()
-      {
-        if (IsInstalled) return;
-        InstallAction?.Invoke();
-      }
-
-      public void TryRemove()
-      {
-        if (!IsInstalled) return;
-        RemoveAction?.Invoke();
-      }
-    }
-
     /// <summary>
     /// Events can't be static because then there will be no way to clear callbacks
     /// So they must be mapped to some throwaway wrapper
     /// </summary>
     private static PluginLifeCycle Instance;
 
-    private static MethodBase GUIDrawMethod => typeof(GUI).GetMethod("Draw");
-    private static MethodBase UpdateMethod => typeof(GameMain).GetMethod("Update", AccessTools.all);
-
-    public static Dictionary<string, Hook> Hooks = new()
-    {
-      ["Stop"] = new Hook()
-      {
-        InstallAction = () =>
-        {
-          GameMain.LuaCs.Hook.Add("stop", $"[{ModInfo.AssemblyName}] PluginLifeCycle.stop", (object[] args) =>
-          {
-            Instance?._stop?.Invoke(); return null;
-          });
-        },
-      },
-
-      ["BeforeGUIDraw"] = new Hook()
-      {
-        InstallAction = () =>
-        {
-          GameMain.LuaCs.Hook.Patch($"[{ModInfo.AssemblyName}] PluginLifeCycle.BeforeGUIDraw", GUIDrawMethod, (instance, ptable) =>
-          {
-            Instance?._beforeGUIDraw?.Invoke((SpriteBatch)ptable["spriteBatch"]);
-            return null;
-          }, LuaCsHook.HookMethodType.Before);
-        },
-      },
-
-      ["AfterGUIDraw"] = new Hook()
-      {
-        InstallAction = () =>
-        {
-          GameMain.LuaCs.Hook.Patch($"[{ModInfo.AssemblyName}] PluginLifeCycle.AfterGUIDraw", GUIDrawMethod, (instance, ptable) =>
-          {
-            Instance?._afterGUIDraw?.Invoke((SpriteBatch)ptable["spriteBatch"]);
-            return null;
-          }, LuaCsHook.HookMethodType.After);
-        },
-      },
-
-      ["AfterUpdate"] = new Hook()
-      {
-        InstallAction = () =>
-        {
-          GameMain.LuaCs.Hook.Patch($"[{ModInfo.AssemblyName}] PluginLifeCycle.AfterUpdate", UpdateMethod, (instance, ptable) =>
-          {
-            Instance?._afterUpdate?.Invoke();
-            return null;
-          }, LuaCsHook.HookMethodType.After);
-        },
-      },
-    };
-
-
-
-
+    public static MethodBase GUIDrawMethod => typeof(GUI).GetMethod("Draw");
+    public static MethodBase UpdateMethod => typeof(GameMain).GetMethod("Update", AccessTools.all);
     static PluginLifeCycle()
     {
       Instance = new PluginLifeCycle();
@@ -110,10 +37,8 @@ namespace BaroJunk
     private static void Clear()
     {
       Instance = null;
-      Hooks = null;
-      // remove hooks?
     }
-    private static void InstallClearHook()
+    public static void InstallClearHook()
     {
       GameMain.LuaCs.Hook.Add("stop", $"[{ModInfo.AssemblyName}] PluginLifeCycle Clear", (object[] args) =>
       {
@@ -121,67 +46,105 @@ namespace BaroJunk
       });
     }
 
+    private static bool StopHookInstalled = false;
+    public static void TryInstallStopHook()
+    {
+      if (StopHookInstalled) return; StopHookInstalled = true;
+      GameMain.LuaCs.Hook.Add("stop", $"[{ModInfo.AssemblyName}] PluginLifeCycle Clear", (object[] args) =>
+      {
+        Clear(); return null;
+      });
+    }
     private event Action _stop; public static event Action Stop
     {
       add
       {
         if (Instance is null) return;
         Instance._stop += value;
-        Hooks["Stop"].TryInstall();
+        TryInstallStopHook();
       }
       remove
       {
         if (Instance is null) return;
         Instance._stop -= value;
-        Hooks["Stop"].TryRemove();
       }
     }
 
+
+    private static bool BeforeGUIDrawHookInstalled = false;
+    public static void TryInstallBeforeGUIDrawHook()
+    {
+      if (BeforeGUIDrawHookInstalled) return; BeforeGUIDrawHookInstalled = true;
+      GameMain.LuaCs.Hook.Patch($"123", GUIDrawMethod, (instance, ptable) =>
+      {
+        Instance?._beforeGUIDraw?.Invoke((SpriteBatch)ptable["spriteBatch"]);
+        return null;
+      }, LuaCsHook.HookMethodType.Before);
+    }
     private event Action<SpriteBatch> _beforeGUIDraw; public static event Action<SpriteBatch> BeforeGUIDraw
     {
       add
       {
         if (Instance is null) return;
         Instance._beforeGUIDraw += value;
-        Hooks["BeforeGUIDraw"].TryInstall();
+        TryInstallBeforeGUIDrawHook();
+
       }
       remove
       {
         if (Instance is null) return;
         Instance._beforeGUIDraw -= value;
-        Hooks["BeforeGUIDraw"].TryRemove();
       }
     }
 
+
+    private static bool AfterGUIDrawHookInstalled = false;
+    public static void TryInstallAfterGUIDrawHook()
+    {
+      if (AfterGUIDrawHookInstalled) return; AfterGUIDrawHookInstalled = true;
+      GameMain.LuaCs.Hook.Patch($"321", GUIDrawMethod, (instance, ptable) =>
+      {
+        Instance?._afterGUIDraw?.Invoke((SpriteBatch)ptable["spriteBatch"]);
+        return null;
+      }, LuaCsHook.HookMethodType.After);
+    }
     private event Action<SpriteBatch> _afterGUIDraw; public static event Action<SpriteBatch> AfterGUIDraw
     {
       add
       {
         if (Instance is null) return;
         Instance._afterGUIDraw += value;
-        Hooks["AfterGUIDraw"].TryInstall();
+        TryInstallAfterGUIDrawHook();
       }
       remove
       {
         if (Instance is null) return;
         Instance._afterGUIDraw -= value;
-        Hooks["AfterGUIDraw"].TryRemove();
       }
     }
 
+    private static bool AfterUpdateHookInstalled = false;
+    public static void TryInstallAfterUpdateHook()
+    {
+      if (AfterUpdateHookInstalled) return; AfterUpdateHookInstalled = true;
+      GameMain.LuaCs.Hook.Patch($"[{ModInfo.AssemblyName}] PluginLifeCycle.AfterUpdate", UpdateMethod, (instance, ptable) =>
+      {
+        Instance?._afterUpdate?.Invoke();
+        return null;
+      }, LuaCsHook.HookMethodType.After);
+    }
     private event Action _afterUpdate; public static event Action AfterUpdate
     {
       add
       {
         if (Instance is null) return;
         Instance._afterUpdate += value;
-        Hooks["AfterUpdate"].TryInstall();
+        TryInstallAfterUpdateHook();
       }
       remove
       {
         if (Instance is null) return;
         Instance._afterUpdate -= value;
-        Hooks["AfterUpdate"].TryRemove();
       }
     }
 

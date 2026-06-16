f5bedda3ccfb0fab906a575bd6486841d5ae6a23
SomeRandomNoobKekeke <SkritniyChel34617@proton.me>
2026-06-16 08:03:43 +0300

    reinventing layouts

---
+++ ClientProject/ClientSource/Experiments/Design/HowLayoutsShouldGenerallyWork/Layouts/Layout.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Diagnostics;
+
+using Microsoft.Xna.Framework;
+using System.Diagnostics;
+
+namespace CrabUIUser
+{
+
+
+  public partial class HowLayoutsShouldGenerallyWork : Experiment
+  {
+    public abstract class Layout
+    {
+      public interface Host
+      {
+        public IReadOnlyList<Child> Children { get; }
+        public Rectangle Rect { get; set; }
+      }
+      public interface ChildBase
+      {
+        public Rectangle Rect { get; set; }
+      }
+      public interface Child : ChildBase, ListLayout.Child, PlainLayout.Child
+      {
+
+      }
+
+
+      public virtual void ConnectTo(Host host)
+      {
+        Parent = host;
+      }
+      private Host Parent;
+
+      public virtual void UpdateChildren()
+      {
+
+      }
+    }
+
+
+
+  }
+}

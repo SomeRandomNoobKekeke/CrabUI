ed5e4aa27fcb59d6e86d1ca3df217bc85de0418a
SomeRandomNoobKekeke <SkritniyChel34617@proton.me>
2026-08-04 09:04:43 +0300

    hiding CUIVisualComponent.LoadFrom in CUIComponent

---
+++ ClientProject/ClientSource/CUI/Core/Components/CUIComponent/CUIComponent.Saving.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Reflection;
+using System.Diagnostics;
+using Barotrauma;
+using Microsoft.Xna.Framework;
+using CUICodeGenerator;
+using CUILibs;
+using System.Xml;
+using System.Xml.Linq;
+using System.IO;
+
+namespace CrabUI
+{
+  public partial class CUIComponent
+  {
+    public new static CUIComponent LoadFrom(string path)
+      => CUIVisualComponent.LoadFrom(path) as CUIComponent;
+  }
+}

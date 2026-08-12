d9fb5b62a5654dc7b4da7e00652f039b39a5acfc
SomeRandomNoobKekeke <SkritniyChel34617@proton.me>
2026-08-12 09:22:50 +0300

    expanded FitContent test, fit in list is borked

--- ClientProject/InMemory/CUITest/Snapshots/Tests/Random/FitContent.cs
+++ ClientProject/InMemory/CUITest/Snapshots/Tests/Random/FitContent.cs
@@ -17,25 +17,130 @@ namespace CrabUIUser
     {
       public static CUIComponent FitContent()
       {
-        CUIFrame frame = new()
+        // Fit in plain layout
+        CUIFrame frame = new CUIDefault.Frame("FitContent")
         {
-          Background = { Color = new Color(32, 32, 32) },
-          Absolute = new CUINullRect(0, 0, 400, 400),
+          Absolute = new CUINullRect(0, 0, 400, 600),
           Anchor = CUIAnchor.Center,
+        };
+
+        frame["container1"] = new CUIComponent()
+        {
+          Absolute = new CUINullRect(x: 40, y: 40),
+          Background = {
+            Sprite = CUISprite.Vignette,
+            Color = new Color(0, 64, 0)
+          },
           FitContent = new CUIBool2(true, true),
         };
 
-        frame["a"] = new CUIComponent()
+        frame["container1"]["a"] = new CUIComponent()
         {
           Background = { Color = Color.Yellow },
-          Absolute = new CUINullRect(20, 20, 30, 30),
+          Absolute = new CUINullRect(20, 20, 10, 10),
         };
 
-        frame["b"] = new CUIComponent()
+        frame["container1"]["b"] = new CUIComponent()
         {
           Background = { Color = Color.Red },
-          Absolute = new CUINullRect(300, 300, 30, 30),
+          Absolute = new CUINullRect(100, 100, 10, 10),
         };
+
+
+        // Fit to nested
+        frame["container2"] = new CUIComponent()
+        {
+          Absolute = new CUINullRect(x: 40, y: 200, h: 50),
+          Background = { Sprite = CUISprite.Vignette, Color = new Color(0, 0, 64) },
+          FitContent = new CUIBool2(true, false),
+        };
+
+        frame["container2"]["a"] = new CUIComponent()
+        {
+          Background = { Sprite = CUISprite.Vignette, Color = new Color(64, 0, 64) },
+          Anchor = CUIAnchor.CenterTop,
+          ParentAnchor = CUIAnchor.CenterBottom,
+          FitContent = new CUIBool2(true, false),
+          Absolute = new CUINullRect(h: 20),
+        };
+
+        frame["container2"]["a"]["b"] = new CUIComponent()
+        {
+          Background = { Sprite = CUISprite.Vignette, Color = new Color(64, 64, 0) },
+          Anchor = CUIAnchor.CenterTop,
+          ParentAnchor = CUIAnchor.CenterBottom,
+          Absolute = new CUINullRect(w: 50, h: 20),
+        };
+
+
+        // Fit to text
+        frame["container3"] = new CUIComponent()
+        {
+          Absolute = new CUINullRect(x: 150, y: 200, h: 50),
+          Background = { Sprite = CUISprite.Vignette, Color = new Color(0, 0, 64) },
+          FitContent = new CUIBool2(true, false),
+        };
+
+        frame["container3"]["a"] = new CUIComponent()
+        {
+          Background = { Sprite = CUISprite.Vignette, Color = new Color(64, 0, 64) },
+          Anchor = CUIAnchor.CenterTop,
+          ParentAnchor = CUIAnchor.CenterBottom,
+          FitContent = new CUIBool2(true, false),
+          Absolute = new CUINullRect(h: 20),
+        };
+
+        frame["container3"]["a"]["b"] = new CUIComponent()
+        {
+          Background = { Sprite = CUISprite.Vignette, Color = new Color(64, 64, 0) },
+          Anchor = CUIAnchor.CenterTop,
+          ParentAnchor = CUIAnchor.CenterBottom,
+          FitContent = new CUIBool2(true, true),
+        };
+
+        frame["container3"]["a"]["b"]["text"] = new CUITextBlock("bruh")
+        {
+          Padding = new CUISizes(4, 10, 4, 10),
+        };
+
+
+        //fit in list
+        frame["container4"] = new CUIComponent()
+        {
+          Absolute = new CUINullRect(x: 250, y: 200, h: 50),
+          Background = { Sprite = CUISprite.Vignette, Color = new Color(0, 0, 64) },
+          FitContent = new CUIBool2(true, false),
+        };
+
+        frame["container4"]["a"] = new CUIVerticalList()
+        {
+          Background = { Sprite = CUISprite.Vignette, Color = new Color(64, 0, 64) },
+          Anchor = CUIAnchor.CenterTop,
+          ParentAnchor = CUIAnchor.CenterBottom,
+          FitContent = new CUIBool2(true, false),
+          Absolute = new CUINullRect(h: 20),
+          // CullChildren = false,
+        };
+
+        frame["container4"]["a"]["b"] = new CUIComponent()
+        {
+          Background = { Sprite = CUISprite.Vignette, Color = new Color(0, 255, 0) },
+          Absolute = new CUINullRect(w: 30, h: 30)
+        };
+
+        frame["container4"]["a"]["c"] = new CUIComponent()
+        {
+          Background = { Sprite = CUISprite.Vignette, Color = new Color(64, 64, 0) },
+          // FitContent = new CUIBool2(true, true),
+          Absolute = new CUINullRect(w: 30, h: 30)
+        };
+
+        frame["container4"]["a"]["c"]["text"] = new CUITextBlock("bruh")
+        {
+          Padding = new CUISizes(4, 10, 4, 10),
+        };
+
+
 
 
         return frame;
